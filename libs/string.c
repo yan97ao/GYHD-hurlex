8923b8e39b31db64f3e19a59f0007647b75298e1
yan97ao <magictao@gmail.com>
2014-05-12 10:33:41 +0800

    implementation of string.h

--- libs/string.c
+++ libs/string.c
@@ -1,37 +1,56 @@
 #include "string.h"
 
-void memcpy(uint8_t dest, const uint8_t *src, uint32_t len)
+void memcpy(uint8_t *dest, const uint8_t *src, uint32_t len)
 {
-
+	while(len--) {
+		*dest++ = *src++;
+	}
 }
 
 void memset(void *dest, uint8_t val, uint32_t len)
 {
+	unsigned char *dst = (unsigned char*)dest;
 
+	while(len--) {
+		*dst++ = val;
+	}
 }
 
 void bzero(void *dest, uint32_t len)
 {
-
+	memset(dest, 0, len);
 }
 
-int strcmp(const char *str1, const char &str2)
+int strcmp(const char *str1, const char *str2)
 {
-
+	while(*str1 && (*str1 == *str2)) {
+		*str1++;
+		*str2++;
+	}
+	return *str1-*str2;
 }
 
 char *strcpy(char *dest, const char *src)
 {
-	
+	char *ret = dest;
+	while( *dest++ = *src++);
+	return ret;
 }
 
 char *strcat(char *dest, const char *src)
 {
-	
+	char *ep = dest;
+	while(*ep)ep++;
+
+	while(*ep++ = *src++);
+
+	return dest;
 }
 
 int strlen(const char *src)
 {
-
+	int len = 0;
+	while(*src++) len++;
+	return len;
 }
 
