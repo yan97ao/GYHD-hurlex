6887dc2c507bc1aee6e602387f426d9a3be9be8f
yan97ao <magictao@gmail.com>
2014-05-14 23:23:29 +0800

    irq

--- idt/idt.c
+++ idt/idt.c
@@ -18,9 +18,21 @@ void isr_handler(pt_regs *regs)
 	}
 }
 
+void irq_handler(pt_regs *regs)
+{
+	if(regs->int_no >= 40) {
+		outb(0xA0, 0x20);
+	}
+	outb(0x20, 0x20);
+
+	if(interrupt_handlers[regs->int_no]) {
+		interrupt_handlers[regs->int_no](regs);
+	}
+}
+
 static void idt_set_gate(uint8_t num, uint32_t base, uint16_t sel, uint8_t flags);
 
-extern idt_flush(uint32_t);
+extern void idt_flush(uint32_t);
 
 void init_idt()
 {
@@ -30,6 +42,21 @@ void init_idt()
 	idt_ptr.base  = (uint32_t)&idt_entries;
 
 	bzero((uint8_t *)&idt_entries, sizeof(idt_entry_t) * 256);
+
+	outb(0x20, 0x11);	//init master 8259
+	outb(0xA0, 0x11);	//init slave 8259
+
+	outb(0x21, 0x20);	//IRQ start from 0x20
+	outb(0xA1, 0x28);	//IRQ start from 0x28
+
+	outb(0x21, 0x04);	//Master <--IR2...INT-- Slave
+	outb(0xA1, 0x02);
+
+	outb(0x21, 0x01);	//8086 mode
+	outb(0xA1, 0x01);
+
+	outb(0x21, 0x0);	//enable
+	outb(0xA1, 0x0);
 
 	idt_set_gate( 0, (uint32_t)isr0,  0x08 ,0x8E);
 	idt_set_gate( 1, (uint32_t)isr1,  0x08 ,0x8E);
@@ -63,6 +90,23 @@ void init_idt()
 	idt_set_gate(29, (uint32_t)isr29, 0x08 ,0x8E);
 	idt_set_gate(30, (uint32_t)isr30, 0x08 ,0x8E);
 	idt_set_gate(31, (uint32_t)isr31, 0x08 ,0x8E);
+
+	idt_set_gate(32, (uint32_t)irq0,  0x08 ,0x8E);
+	idt_set_gate(33, (uint32_t)irq1,  0x08 ,0x8E);
+	idt_set_gate(34, (uint32_t)irq2,  0x08 ,0x8E);
+	idt_set_gate(35, (uint32_t)irq3,  0x08 ,0x8E);
+	idt_set_gate(36, (uint32_t)irq4,  0x08 ,0x8E);
+	idt_set_gate(37, (uint32_t)irq5,  0x08 ,0x8E);
+	idt_set_gate(38, (uint32_t)irq6,  0x08 ,0x8E);
+	idt_set_gate(39, (uint32_t)irq7,  0x08 ,0x8E);
+	idt_set_gate(40, (uint32_t)irq8,  0x08 ,0x8E);
+	idt_set_gate(41, (uint32_t)irq9,  0x08 ,0x8E);
+	idt_set_gate(42, (uint32_t)irq10, 0x08 ,0x8E);
+	idt_set_gate(43, (uint32_t)irq11, 0x08 ,0x8E);
+	idt_set_gate(44, (uint32_t)irq12, 0x08 ,0x8E);
+	idt_set_gate(45, (uint32_t)irq13, 0x08 ,0x8E);
+	idt_set_gate(46, (uint32_t)irq14, 0x08 ,0x8E);
+	idt_set_gate(47, (uint32_t)irq15, 0x08 ,0x8E);
 
 	idt_set_gate(255, (uint32_t)isr255, 0x08, 0x8E);
 
--- include/idt.h
+++ include/idt.h
@@ -79,4 +79,42 @@ void isr30();
 void isr31();
 
 void isr255();
+
+void irq_handler(pt_regs *regs);
+
+#define IRQ0	32
+#define IRQ1	33
+#define IRQ2	34
+#define IRQ3	35
+#define IRQ4	36
+#define IRQ5	37
+#define IRQ6	38
+#define IRQ7	39
+#define IRQ8	40
+#define IRQ9	41
+#define IRQ10	42
+#define IRQ11	43
+#define IRQ12	44
+#define IRQ13	45
+#define IRQ14	46
+#define IRQ15	47
+
+void irq0();
+void irq1();
+void irq2();
+void irq3();
+void irq4();
+void irq5();
+void irq6();
+void irq7();
+void irq8();
+void irq9();
+void irq10();
+void irq11();
+void irq12();
+void irq13();
+void irq14();
+void irq15();
+
+
 #endif /* end of include guard */
